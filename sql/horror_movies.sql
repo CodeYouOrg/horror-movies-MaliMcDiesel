3144b1db3ddad3d3018f591ba3e08baf3840a005
Malachi McDaniel <malachim208@icloud.com>
2024-10-12 11:46:00 -0400

    Finished

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,8 @@
--- Add your SQL here
+SELECT m.id AS Movie_ID, 
+m.name AS Movie_Title, 
+m.imdb_rating AS Rating
+FROM movies m
+WHERE genre = 'horror'
+    AND year <= '1985'
+ORDER BY m.imdb_rating DESC
+LIMIT 3;
